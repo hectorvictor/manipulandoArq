0ab69f504871de7c0806e4c9fd2610acbe37029f
hectorvictor <victohector@gmail.com>
2021-05-15 14:35:29 -0300

    calculando o tamanho total de bytes dos arquivos

--- src/main/java/br/pratica/manipulandoarquivos/AppFile.java
+++ src/main/java/br/pratica/manipulandoarquivos/AppFile.java
@@ -3,17 +3,33 @@ package br.pratica.manipulandoarquivos;
 import java.io.File;
 import java.io.IOException;
 
+import javax.swing.text.html.HTMLDocument.HTMLReader.TagAction;
+
 public class AppFile {
     public static void main(String[] args) throws IOException {
-        listarArquivosDiretorio();
+        File diretorio = new File("/home/hector/Documentos");
+        listarArquivosDiretorio(diretorio);
+        long tamanhoEmBytesDiretorio = calcularTamanhoBytesDoDiretorio(diretorio);
+        System.out.println("Tamanho total:" + tamanhoEmBytesDiretorio + " bytes");
       
     }
 
-    private static void listarArquivosDiretorio() {
-        File diretorio = new File("/home/hector/Documentos");
-        File[] nome = diretorio.listFiles();
+    private static long calcularTamanhoBytesDoDiretorio(File diretorio) {
+        long tamanho = 0;
+        File[] arquivo = diretorio.listFiles();
 
-        for(File p: nome) {
+        for(File p: arquivo) {
+            tamanho += arquivo.length;
+        }     
+
+
+        return tamanho;
+    }
+
+    private static void listarArquivosDiretorio(File diretorio) {
+        File[] arquivo = diretorio.listFiles();
+
+        for(File p: arquivo) {
             System.out.println(p.getName()+ "- tamanho: "+ p.length() + " Bytes");
 
         }
