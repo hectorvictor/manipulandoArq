4cdb0f3e80795f1bbeae4883e210e42e5cf5b288
hectorvictor <victohector@gmail.com>
2021-05-22 16:19:40 -0300

    Aprendendo a ler arquivos usando (FileReader)

--- src/main/java/br/pratica/estudo20210555/AppArquivo.java
+++ src/main/java/br/pratica/estudo20210555/AppArquivo.java
@@ -1,6 +1,8 @@
 package br.pratica.estudo20210555;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -10,18 +12,34 @@ public class AppArquivo {
         File arquivo = new File("/home/hector/Documentos/teste");
         //criarPasta(arquivo);
         //escreverArquivos(arquivo);
+        lerArquivo(arquivo);
         
 
 
     }
 
+    private static void lerArquivo(File arquivo) throws IOException {
+        FileReader ler = new FileReader(arquivo);
+        char[] in = new char[6];
+        int size = ler.read(in);
+        int i = 0;
+        while(size != -1) {
+            System.out.print(in);
+            size = ler.read();
+            i++;
+        }
+        
+        System.out.println(i);
+
+        
+        
+    }
+
     private static void escreverArquivos(File arquivo) throws IOException {
         FileWriter escrever = new FileWriter(arquivo);
 
-        for(int i=0; i< 100; i++) {
-            escrever.write("Hector\n");
+        escrever.write("Hector\n");
 
-        }
         escrever.flush();
         escrever.close();
         
